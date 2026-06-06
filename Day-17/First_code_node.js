36ada010df93c1bf4d645536df34c896acf3cd15
Divyansh Rawal <divyanshmrawal@gmail.com>
2026-06-06 11:32:44 +0530

    Set up basic HTTP server in First_code_node.js

---
+++ Day-17/First_code_node.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+http.createServer((req, res)=> {
+    res.end('Hello World!');
+    let number = 5;
+
+    if (number % 2 === 0) {
+        console.log(`${number} is an Even number.`);
+    } else {
+        console.log(`${number} is an Odd number.`);
+    };
+}).listen(8080)
+
